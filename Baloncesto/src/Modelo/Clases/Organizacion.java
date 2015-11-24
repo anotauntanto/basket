7d878264c19745fab83d1bc9ee5ea6480648b6f0
inftel05 <inftel05@192.168.183.29>
2015-11-24 13:15:31 +0100

    Añadido imagen en cena

--- Baloncesto/src/Modelo/Clases/Organizacion.java
+++ Baloncesto/src/Modelo/Clases/Organizacion.java
@@ -8,21 +8,21 @@ package Modelo.Clases;
 import java.util.Date;
 
 /**
- *
- * @author grupo_baloncesto, esta clase representa la entidad Organización
+ * Esta clase representa la entidad Organización
+ * @author grupo_baloncesto
  */
 public class Organizacion extends Persona{
     /**
-     * 
-     * @param idPersona contructor de la clase Organización con los atributos que deriva de la clase persona
-     * @param nombre
-     * @param apellidos
-     * @param dni
-     * @param fechaN
-     * @param email
-     * @param contrasena
-     * @param tlf
-     * @param rol 
+     * contructor de la clase Organización con los atributos que deriva de la clase persona
+     * @param idPersona int identificador de persona
+     * @param nombre String nombre
+     * @param apellidos String apellidos
+     * @param dni String dni
+     * @param fechaN Date fecha de nacimiento
+     * @param email String email de la organización
+     * @param contrasena String contraseña de la organización
+     * @param tlf String telefono
+     * @param rol int rol, identificador de la clase organización
      */
 
     public Organizacion(int idPersona,String nombre, String apellidos, String dni, Date fechaN, String email, String contrasena, String tlf, int rol) {
@@ -37,7 +37,7 @@ public class Organizacion extends Persona{
     }
     /**
      * 
-     * @param numJornada 
+     * @param numJornada int numero de jornada
      */
     
     //Organiza la jornada siguiente
@@ -46,8 +46,8 @@ public class Organizacion extends Persona{
     }
     /**
      * 
-     * @param idJugador
-     * @param idPartido 
+     * @param idJugador int identificador de jugador
+     * @param idPartido int identificador de partido
      */
     
     //Guardo en la tabla de estadisticas
@@ -56,7 +56,7 @@ public class Organizacion extends Persona{
     }
     /**
      * 
-     * @param idPartido 
+     * @param idPartido int identificador de partido
      */
     //Fija el resultado del partido dado y fijo estadisicas
     public void setResultado(int idPartido){
