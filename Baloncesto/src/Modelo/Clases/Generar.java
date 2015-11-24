6cbddd2d5120792371e72941ae40bf015a29c634
inftel08 <inftel08@192.168.183.43>
2015-11-24 12:19:48 +0100

    Merge origin/master
    Conflicts:
    	public_html/css/cenas.css

---
+++ Baloncesto/src/Modelo/Clases/Generar.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.Clases;
+
+import Modelo.DAO.EquipoDAO;
+import Modelo.DAO.PartidoDAO;
+import Modelo.DAO.PartidoJugadoDAO;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+/**
+ *
+ * @author inftel06
+ */
+public class Generar {
+    private ArrayList miArray;  
+    private int[] arrayUno,arrayDos;
+    private int i,j;
+    public Generar(){
+        
+    }
+    public void sigJornada(){
+        miArray=(ArrayList) EquipoDAO.obtenerTodosEquipos();
+        i=0;j=0;
+        Collections.shuffle(miArray);
+        for (Iterator it = miArray.iterator(); it.hasNext();) {
+            Equipo elem = (Equipo) it.next();
+            if (elem.getCategoria()==1){
+                arrayUno[i]=elem.getIdEquipo();
+                i++;
+            }
+            else {
+                arrayDos[j]=elem.getIdEquipo();
+                j++;
+            }
+        }
+        
+        
+        
+             
+    }
+    
+    public int sigJornadaN(){
+        int jornada=PartidoDAO.obtenerJornadaActual();
+        //ArrayList arrayPartidos=(ArrayList) PartidoJugadoDAO.obtenerTodosPartidosJugados();
+        ArrayList arrayPartidos=(ArrayList) PartidoDAO.listarPartidosJornada(jornada);
+        for (Iterator it=arrayPartidos.iterator();it.hasNext();){
+            Partido elem=(Partido) it.next();
+            if (elem.getResultado().equals("0")){
+                 return 1; //Sale si la jornada actual no ha finalizado
+                 
+            }
+        }
+        
+        
+    }
+    
+}
+
+ 
--- Baloncesto/src/Modelo/DAO/PartidoDAO.java
+++ Baloncesto/src/Modelo/DAO/PartidoDAO.java
@@ -19,16 +19,16 @@ import java.util.List;
  * @author inftel07
  */
 public class PartidoDAO {
-    private static List <Partido> listaPartidos=null;
-    static Connection con=null;
-    
-    
-    public static void crearPartido (Partido par) {
+
+    private static List<Partido> listaPartidos = null;
+    static Connection con = null;
+
+    public static void crearPartido(Partido par) {
         con = Conexion.conectar();
         //persona = per;
-        
-        try{
-            PreparedStatement ps =  con.prepareStatement("insert into Partido (Id_Partido, Resultado, Jornada, Id_Arbitro, Fecha, Localizacion)"
+
+        try {
+            PreparedStatement ps = con.prepareStatement("insert into Partido (Id_Partido, Resultado, Jornada, Id_Arbitro, Fecha, Localizacion)"
                     + " values (INC_ID_PARTIDO.NextVal,?,?,?,?,?)");
             //ps.setInt(1, per.getIdPersona());
             ps.setString(1, par.getResultado());
@@ -37,75 +37,91 @@ public class PartidoDAO {
             ps.setDate(4, new java.sql.Date(par.getFecha().getTime()));
             ps.setString(5, par.getLocalizacion());
             ps.executeUpdate();
-            
-            
-        }catch(SQLException ex){
+
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
         //Conexion.desconexion();
     }
-    
-    public static List<Partido> listarPartidosJornada (int num_jornada) { //para emparejamientos
+
+    public static List<Partido> listarPartidosJornada(int num_jornada) { //para emparejamientos
         con = Conexion.conectar();
-        listaPartidos= new ArrayList<>();
-        Partido partido = null; 
-        
-        try{
+        listaPartidos = new ArrayList<>();
+        Partido partido = null;
+
+        try {
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from Partido par where par.Jornada = "+num_jornada);
-  
+            ResultSet rs = st.executeQuery("select * from Partido par where par.Jornada = " + num_jornada);
+
             //ResultSetMetaData rsmd = rs.getMetaData();
             while (rs.next()) {
-                partido = new Partido (rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4) ,rs.getDate(5), rs.getString(6));
-                
+                partido = new Partido(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getDate(5), rs.getString(6));
+
                 listaPartidos.add(partido);
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-        
+
         return listaPartidos;
-        
+
     }
-    
-    public static Partido obtenerDatosPartido (int id_partido) { 
+
+    public static Partido obtenerDatosPartido(int id_partido) {
         con = Conexion.conectar();
-        Partido partido = null; 
-        
-        try{
+        Partido partido = null;
+
+        try {
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from Partido where Id_Partido = "+id_partido);
-  
+            ResultSet rs = st.executeQuery("select * from Partido where Id_Partido = " + id_partido);
+
             //ResultSetMetaData rsmd = rs.getMetaData();
             while (rs.next()) {
-                partido = new Partido (rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4) ,rs.getDate(5), rs.getString(6));
-                
+                partido = new Partido(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getDate(5), rs.getString(6));
 
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-        
+
         return partido;
-        
+
     }
-    
- 
+
+    public static int obtenerJornadaActual() {
+        con = Conexion.conectar();
+        //listaPersonas= new ArrayList<>();
+        int jornadaAct = 0;
+        try {
+            PreparedStatement ps = con.prepareStatement("select max(jornada) from partido");
+
+            ResultSet rs = ps.executeQuery();
+            //ResultSetMetaData rsmd = rs.getMetaData();
+            //int number = rsmd.getColumnCount();
+            while (rs.next()) {
+                jornadaAct = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return jornadaAct;
+    }
+
     public static void modificarResultado(Partido par) {
         con = Conexion.conectar();
-        
-        try{
-            PreparedStatement ps =  con.prepareStatement("update Partido set Resultado=? where id_Partido = ?");
-            
+
+        try {
+            PreparedStatement ps = con.prepareStatement("update Partido set Resultado=? where id_Partido = ?");
+
             ps.setString(1, par.getResultado());
             ps.setInt(2, par.getIdPartido());
             ps.executeUpdate();
-                
-        }catch(SQLException ex){
+
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
         //Conexion.desconexion();
     }
-    
-    
+
 }
